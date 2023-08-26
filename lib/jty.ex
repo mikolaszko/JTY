8aaff11ca4313bfc8d0fc7e22e9876ad388ae5a0
mikolaszko <mikosodz@gmail.com>
2023-08-26 19:37:43 +0200

    Working writes to the file

--- lib/jty.ex
+++ lib/jty.ex
@@ -10,23 +10,30 @@ defmodule JTY do
 
       if :ok then yaml file gets generated
   """
-  def parse(path) do
-    open_file_and_trim(path)
+  def parse(path, dest) do
+    open_file_and_trim(path) |> create_yaml_file(dest)
+    
+     
+  end
+
+  defp create_yaml_file(content, dest) do
+    IO.inspect(dest)
+    IO.inspect(content)
+
+    File.write!(dest, IO.iodata_to_binary(content))
   end
 
   defp open_file_and_trim(path) do
-    opened_tupled = File.stream!(path) |> Enum.with_index
-    trimmed = Enum.map(
+    opened_tupled = File.stream!(path, [:raw]) |> Enum.with_index
+    Enum.map(
       opened_tupled,
       fn {line, i}-> 
         cond do
-           i == 0 -> nil
-           i == length(opened_tupled)-1 -> nil
-           true -> String.trim(line)
+           i == 0 -> "start" 
+           i == length(opened_tupled)-1 -> "end"
+           true -> String.trim(line) 
+            |> String.replace(~s("), "") 
         end
-
-    end) 
-    IO.inspect(trimmed)
-    trimmed
+    end)
   end
 end
