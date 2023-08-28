f6c87d40b42dd0fa52c40b2a6e5a626e78c776aa
mikolaszko <mikosodz@gmail.com>
2023-08-28 21:33:53 +0200

    Beginnings of tokenazation and string walk

--- lib/jty.ex
+++ lib/jty.ex
@@ -3,23 +3,32 @@ defmodule JTY do
   Simple parser from JSON file to YAML file
   """
 
-  @doc """
-  ## Examples
-      iex> JTY.parse(path)
-      :ok || :error
+  defguardp is_letter(c) when c in ?a..?z or c in ?A..?Z or c == ?_
+  defguardp is_digit(c) when c in ?0..?9
+  defguardp is_whitespace(c) when c in ~c[ ] 
 
-      if :ok then yaml file gets generated
-  """
   def parse(path, dest) do
-    open_file_and_trim(path) |> create_yaml_file(dest)
-    
-     
+    open_file_and_trim(path) |> reformat([]) |> create_yaml_file(dest)
+  end
+
+
+  defp reformat(input, tokens) do
+    Enum.map(input, 
+      fn item -> 
+          String.graphemes(item) |> 
+          Enum.map(
+            fn c ->
+              case c do
+                ":" -> tokenize(c)
+                _ -> c
+              end
+          end) |>
+          IO.puts()
+          item
+    end)
   end
 
   defp create_yaml_file(content, dest) do
-    IO.inspect(dest)
-    IO.inspect(content)
-
     File.write!(dest, IO.iodata_to_binary(content))
   end
 
@@ -29,11 +38,13 @@ defmodule JTY do
       opened_tupled,
       fn {line, i}-> 
         cond do
-           i == 0 -> "start" 
-           i == length(opened_tupled)-1 -> "end"
+           i == 0 -> "---\n" 
+           i == length(opened_tupled)-1 -> "\n..."
            true -> String.trim(line) 
             |> String.replace(~s("), "") 
         end
     end)
   end
+
+  defp tokenize(<<":">>), do: "\n"
 end
